01cfd71da91d4729900653dbdf946cb901f328bf
arkhanlk <44444673+arkhanlk@users.noreply.github.com>
2024-02-08 19:45:14 -0300

    Update Hello.py
    main file

--- Hello.py
+++ Hello.py
@@ -1,51 +1,82 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 
 import streamlit as st
-from streamlit.logger import get_logger
+import json
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import tika
+tika.initVM()
+from tika import parser
+from deep_translator import GoogleTranslator
+from stqdm import stqdm
+import string
+import re
+import time
+import math
 
-LOGGER = get_logger(__name__)
+st.set_page_config(
+    page_title='Xtraiduz - Extrai & Traduz',
+    page_icon='📖',)
 
+#### CODE AFTER THIS LINE ####
+tbET, = st.tabs(['Extrair & Traduzir'])
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# gerando arquivo
+with tbET:
+    st.write('# Faça upload de um arquivo 📖 (ePub ou PDF)')
+#    st.sidebar.success('Escolha uma função acima')
 
-    st.write("# Welcome to Streamlit! 👋")
+    uploaded_file = st.file_uploader('Escolher arquivo', accept_multiple_files=False)
+    if uploaded_file:
+        
+        out = 'original.txt'
+        parsed = parser.from_file(uploaded_file)
+        content = parsed['content']
+        with open(out, 'w', encoding='utf-8') as fout:
+            fout.write(content)
+            fout.close()
+        f = open('original.txt', 'r', encoding='utf8')
+        
+        if 'original' not in st.session_state:
+            st.session_state['original'] = f
 
-    st.sidebar.success("Select a demo above.")
+        lines = f.readlines()
+        c = 0
+        z = str()
+        with st.expander('Prévia do texto'):
+            for line in lines:
+                if len(line) >= 3:
+                    c += len(line)
+                    z += (line + '\n')
+                    st.markdown(line)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+        st.download_button(
+            label='Download texto extraído',
+            data=z,
+            file_name=out,
+            mime='text')
 
+        st.info(f'Quantidade de letras nesse texto, considerando os espaços: {c}', icon='ℹ️')
+        st.warning('O máximo de letras permitidas por dia são 300000', icon='⚠️')
 
-if __name__ == "__main__":
-    run()
+        trad = st.button('Traduzir!', key='traduzido')
+        if trad:
+            traduzido = []
+            for line in stqdm(lines):
+                time.sleep(0.01)
+                if len(line) >= 3:
+                    translated = GoogleTranslator(source='auto', target='pt').translate(line)
+                    traduzido.append(translated)
+
+            zz = str()
+            with st.expander('Prévia da tradução'):
+                for ln in traduzido:
+                    zz += str(ln + '\n')
+                    st.markdown(ln + '\n')
+            
+            st.download_button(
+            label='Download texto traduzido',
+            data=zz,
+            file_name='traduzido.txt',
+            mime='text')
