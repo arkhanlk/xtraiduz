764703f1481c88cfb2b278daab1e1223d9b872cf
arkhanlk <44444673+arkhanlk@users.noreply.github.com>
2024-12-09 10:44:54 -0300

    Update Natal.py

--- pages/Natal.py
+++ pages/Natal.py
@@ -3,6 +3,40 @@ from streamlit import session_state as ss
 import pandas as pd
 import random 
 
+"""
+part = {
+    'Estela':'',
+    'JPedro':'',
+    'Luiz':'',
+    'Karina':'',
+    'JPaulo':'',
+    'Duda':'',
+    'Eliana':'',
+    'Vitor':'',
+    'Mila':'',
+    'Iolanda':'',
+    'Alessandra':'',
+    'Paulinho':'',
+    }
+
+dice = list(part.keys())
+random.shuffle(dice)
+
+diced = {}
+def rander():
+    for k, d in zip(list(part.keys()), dice):
+        if k != d:
+            diced[k] = [ord(c) for c in list(d)]
+            st.success(k)
+        else:
+            st.warning(k)
+    st.write(diced)
+
+z = st.button('rand')
+if z:
+    rander()
+""";
+
 ss['txt'] = st.text_area('Cola o código aqui')
 ss['btn'] = st.button('Clique para decifrar')
 
