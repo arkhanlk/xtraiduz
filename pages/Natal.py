9d98bf2f1ae52d303b05031ebaacf3ab19a05a41
arkhanlk <44444673+arkhanlk@users.noreply.github.com>
2024-12-09 09:03:12 -0300

    Update Natal.py

--- pages/Natal.py
+++ pages/Natal.py
@@ -5,7 +5,7 @@ import pandas as pd
 ss['txt'] = st.text_area('Cola o código aqui')
 ss['btn'] = st.button('Clique para decifrar')
 def word_count(str):
-    res = ''.join(format(ord(i), '08b') for i ss['txt'])
+    res = ''.join(format(ord(i), '08b') for i in ss['txt'])
     
 # Call the word_count function with an input sentence and print the results.
 if button:
